f916eafea749cf42b04fb1abe3b0175c62534445
Michael Padiernos <mike.padiernos@gmail.com>
2020-04-18 09:34:03 -0400

    [FIX] Routers, Seeds / .post, tasks cleaning
    - removed unnecessary spaces
    - fixed .post for projects, resources,& tasks routers

--- api/routers/resources-router.js
+++ api/routers/resources-router.js
@@ -5,35 +5,6 @@ const db = require('../../data/db-config');
 router
 	.route('/')
 	.get((req, res) => {
-		db('projects')
-			.then(project => {
-				res.json(project)
-			})
-			.catch(error => {
-				res.status(500).json({success: false, message: "Unable to retrieve projects", error})
-			})
-	})
-	.post((req, res) => {});
-
-router
-	.route('/:id')
-	.get((req, res) => {})
-	.put((req, res) => {})
-	.delete((req, res) => {});
-
-router
-	.route('/:id/resources')
-	.get((req, res) => {})
-	.post((req, res) => {});
-
-router
-	.route('/:id/tasks')
-	.get((req, res) => {})
-	.post((req, res) => {});
-
-router
-	.route('/resources')
-	.get((req, res) => {
 		db('resources')
 			.then(resource => {
 				res.json(resource)
@@ -42,31 +13,12 @@ router
 				res.status(500).json({success: false, message: "Unable to retrieve resources", error})
 			})
 	})
-	.post((req, res) => {});
-
-router
-	.route('/resources/:id')
-	.get((req, res) => {})
-	.put((req, res) => {})
-	.delete((req, res) => {});
-
-router
-	.route('/tasks')
-	.get((req, res) => {
-		db('tasks')
-			.then(task => {
-				res.json(task)
-			})
-			.catch(error => {
-				res.status(500).json({success: false, message: "Unable to retrieve tasks", error})
-			})
-	})
 	.post((req, res) => {
 		const resource = req.body;
-		db('projects')
+		db('resources')
 			.insert(resource)
 			.then(id => {
-				console.log("resource: ",id);
+				console.log("resource: ", id);
 				res.json({success: true, message: "Resource added", id: id, resource})
 			})
 			.catch(error => {
@@ -75,9 +27,10 @@ router
 	});
 
 router
-	.route('/tasks/:id')
+	.route('/:id')
 	.get((req, res) => {})
 	.put((req, res) => {})
 	.delete((req, res) => {});
+
 
 module.exports = router;
